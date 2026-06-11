c50b224384bf390668eb623627818d9f266a48c6
Artyom Veremeenko <arwer13@gmail.com>
2026-06-11 09:48:52 +0200

    chore: bump solidity to 0.8.34 and osaka

--- src/SyncTrigger.sol
+++ src/SyncTrigger.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.34;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
--- src/interfaces/ISyncTrigger.sol
+++ src/interfaces/ISyncTrigger.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.20;
+pragma solidity 0.8.34;
 
 interface ISyncTrigger {
     error SyncTriggerInvalidAmounts(uint128 minAmount, uint128 maxAmount);
